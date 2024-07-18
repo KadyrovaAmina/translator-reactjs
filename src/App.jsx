54b25cf1bd8049eed16c5043be20a77281e4a8f6
Amina Kadyrova <kadyrovaaminadev@gmail.com>
2024-07-18 14:51:45 +0600

    feat: add ability to record a voice and upload files, also stop recording

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,36 @@
+import { useState } from "react";
+import { Header } from "./components/Header";
+import { HomePage } from "./components/HomePage";
+import { FileDisplay } from "./components/FileDisplay";
+
 function App() {
+  const [file, setFile] = useState(null);
+  const [audioStream, setAudioStream] = useState(null);
+
+  const isAudioAvailable = file || audioStream;
+
+  const handleAudioReset = () => {
+    setFile(null);
+    setAudioStream(null);
+  };
 
   return (
-    <div></div>
-  )
+    <div className="flex flex-col max-w-[1000px] mx-auto w-full">
+      <section className="min-h-screen flex flex-col">
+        <Header />
+        {isAudioAvailable ? (
+          <FileDisplay
+            file={file}
+            audioStream={audioStream}
+            handleAudioReset={handleAudioReset}
+          />
+        ) : (
+          <HomePage setFile={setFile} setAudioStream={setAudioStream} />
+        )}
+      </section>
+      <footer></footer>
+    </div>
+  );
 }
 
-export default App
+export default App;
